a4df26cea1837e975eb06947ccebba519df6d5fb
Lori M Olson <lori@wndx.com>
2015-02-17 20:44:44 -0700

    Add examples for sphero demo

---
+++ sphero.rb
@@ -0,0 +1,12 @@
+require 'artoo'
+
+connection :sphero, :adaptor => :sphero, :port => '/dev/tty.Sphero-YYO-AMP-SPP' # mac   'COM4' # windows
+device :sphero, :driver => :sphero
+
+work do
+  every(1.seconds) do
+    puts "Rolling..."
+    sphero.set_color(rand(255),rand(255),rand(255))
+    sphero.roll 20, rand(360)
+  end
+end
