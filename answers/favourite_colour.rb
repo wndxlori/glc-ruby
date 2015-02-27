5282a90c2e93ba7538ec9867c82648bfd7bbf299
Lori M Olson <lori@wndx.com>
2015-02-27 11:39:59 -0700

    Some additions from actually running the class

---
+++ answers/favourite_colour.rb
@@ -0,0 +1,8 @@
+puts "Your favorite color?"
+colour = gets.chomp
+
+if colour == "blue"
+  puts "Yay! that's my favourite color, too!"
+else
+  puts "Ummm... ok, I guess." 
+end
--- sphero.rb
+++ sphero.rb
@@ -7,6 +7,6 @@ work do
   every(3.seconds) do
     puts "Rolling..."
     sphero.set_color(rand(255),rand(255),rand(255))
-    sphero.roll 20, rand(360)
+    sphero.roll 100, rand(360)
   end
 end
